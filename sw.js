ededb3af4700c5743c349e7b74d2e2d0974d6906
Ada Rose Edwards <ada@ada.is>
2017-02-16 13:05:22 +0000

    fix cachefirst

--- sw.js
+++ sw.js
@@ -1,17 +1,17 @@
 /* global toolbox, importScripts, self */
 /* jshint browser:true */
-'use strict';
+/use strict/;
 
-importScripts('sw-toolbox.js');
+importScripts(/sw-toolbox.js/);
 
 toolbox.options.networkTimeoutSeconds = 3;
 
-toolbox.router.get('https://cdn.polyfill.io/*', toolbox.cacheFirst);
-toolbox.router.get('https://aframe.io/*', toolbox.cacheFirst);
-toolbox.router.get('https://cdn.rawgit.com/*', toolbox.cacheFirst);
-toolbox.router.get('https://rawgit.com/*', toolbox.cacheFirst);
-toolbox.router.get('https://cdnjs.cloudflare.com/*', toolbox.cacheFirst);
-toolbox.router.get('https://ajax.googleapis.com/*', toolbox.cacheFirst);
-toolbox.router.get('https://samsunginter.net/fonts/*', toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/cdn.polyfill.io\/.*/ig, toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/aframe.io\/.*/ig, toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/cdn.rawgit.com\/.*/ig, toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/rawgit.com\/.*/ig, toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/cdnjs.cloudflare.com\/.*/ig, toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/ajax.googleapis.com\/.*/ig, toolbox.cacheFirst);
+toolbox.router.get(/https:\/\/samsunginter.net\/fonts\/.*/ig, toolbox.cacheFirst);
 
 toolbox.router.default = toolbox.networkFirst;
