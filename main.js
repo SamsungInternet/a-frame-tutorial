a017c4837d5ec08bb345356eedb812c9c178526e
Ada Rose Edwards <ada@ada.is>
2017-02-16 11:48:51 +0000

    perf

--- main.js
+++ main.js
@@ -74,3 +74,18 @@ function prevStep() {
 }
 document.getElementById('btn-prevstep').addEventListener('click', prevStep);
 
+
+// Fancy Type Faces
+window.WebFontConfig = {
+	custom: {
+		families: ['Samsung One:n4,n7'],
+		urls: ['https://samsunginter.net/fonts/SamsungOne/fonts.css']
+	}
+};
+
+(function(d) {
+	var wf = d.createElement('script')
+	var s = d.scripts[0];
+	wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
+	s.parentNode.insertBefore(wf, s);
+} (document));
